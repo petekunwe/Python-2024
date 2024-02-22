bd8f42c1d13c035d97289b6a8e6046bfa35c0908
Peter Ekunwe <etinp@hotmail.com>
2024-02-22 06:36:28 -0700

    Initial Commit

---
+++ Hello.py
@@ -0,0 +1,27 @@
+# CLear the Terminal Screen
+import os
+os.system('clear')
+
+################################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+################################
+
+
+
+first_name = 'Peter'
+age = 21
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"
+}
+name = True
+print (name)
+
+
